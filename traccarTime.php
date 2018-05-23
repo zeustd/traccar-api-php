88c9f723c4503457fd7b3dfb50f9df8420eb8012
James <jamejo@gmail.com>
2018-05-23 19:19:29 -0400

    Update traccarTime.php

--- traccarTime.php
+++ traccarTime.php
@@ -1,4 +1,19 @@
 <?php
+/*
+ * Copyright 2018 James Joseph (james@yodiyil.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 class traccarTime {
 
@@ -21,13 +36,13 @@ class traccarTime {
     return $returnIso;
     }
 
-
-    
-
 }
 
-
-//Format to submit time in toIso function :  2017-07-25 00:00:10
-//format of time zone :  Asia/Kolkata
-//This function takes the user input time and timezone and converts it into UTC time and returns a response ISO Time format.
+/*
+ * ReadMe / Instructions & Notes
+ *
+ * Date time Format to submit time in toIso function :  2017-07-25 00:00:10
+ * Format of time zone :  Asia/Kolkata
+ * This function takes the user input time and timezone and converts it into UTC time and returns a response ISO Time format.
+*/
 ?>
