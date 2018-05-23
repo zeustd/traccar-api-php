481195172855e9500533a9b647bceadee35faf2f
James <jamejo@gmail.com>
2018-05-23 19:27:09 -0400

    Update reportsTrips.php

--- reportsTrips.php
+++ reportsTrips.php
@@ -1,14 +1,13 @@
 <?php
+
 include('incPostLogin.php');
 
 $deviceId = $_REQUEST['deviceId'];
 $fromTime = $_REQUEST['from'];
 $toTime = $_REQUEST['to'];
 $timeZone = $_REQUEST['timeZone'];
-
 $from = traccarTime::toIso($fromTime, $timeZone);
 $to = traccarTime::toIso($toTime, $timeZone);
-
 
 $reportTrips=traccar::reportTrips($deviceId,$from,$to,$cookie);
 
@@ -22,13 +21,7 @@ if($reportTrips->responseCode=='200') {
 	$out['sessionResponse'] = $sessionResponse;
 	$out['sessionResponseCode'] = $sessionResponseCode;
 	$out['response'] = $reportTripsArray;
-	
-	
-	//$out['response']['computedSpeed'] = $positionsArray[1]['protocol'];
-	
 	$rows[] = $out;
-	
-
 }else{
 	$out['apiResponse'] = 'error';
 	$out['apiresponseCode'] = $reportTrips->responseCode;
@@ -36,11 +29,9 @@ if($reportTrips->responseCode=='200') {
 	$out['sessionResponseCode'] = $sessionResponseCode;
 	$out['response'] = $reportTrips->response;
 	$rows[] = $out;
-	
-
 }
 
-	$results = array('data' => $rows);
-	echo json_encode($results);
+$results = array('data' => $rows);
+echo json_encode($results);
 
 ?>
