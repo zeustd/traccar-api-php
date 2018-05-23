4b500717b213956f07ac93c9a919b8af9d23beb4
James <jamejo@gmail.com>
2018-05-23 19:28:53 -0400

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,4 +1,5 @@
 <?php
+
 include('incPostLogin.php');
 
 $login=traccar::logout($cookie);
@@ -6,26 +7,20 @@ $login=traccar::logout($cookie);
 $rows = array();
 
 if($login->responseCode=='204') {
-	
 	$response = $login->response;
 	$userArray = json_decode($response,true);
 	$out['apiResponse'] = 'ok';
 	$out['apiResponseCode'] = $login->responseCode;
 	$out['response'] = $userArray;
-	
 	$rows[] = $out;
-
 }else{
 	$out['apiResponse'] = 'error';
 	$out['apiresponseCode'] = $login->responseCode;
 	$out['response'] = $login->response;
-	
 	$rows[] = $out;
-	
-
 }
 
-	$results = array('data' => $rows);
-	echo json_encode($results);
+$results = array('data' => $rows);
+echo json_encode($results);
 
 ?>
