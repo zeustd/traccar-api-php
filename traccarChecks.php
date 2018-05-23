6ac1a9a58993b0c9982c3eced3fd9f9039e71060
James <jamejo@gmail.com>
2018-05-23 19:16:32 -0400

    Update traccarChecks.php

--- traccarChecks.php
+++ traccarChecks.php
@@ -3,16 +3,14 @@
 $cookie = $_GET['cookie'];
 
 $t=traccar::session($cookie);
-
 if($t->responseCode=='200') {
-	
+
 	$sessionResponse = 'ok';
 	$sessionResponseCode = $t->responseCode;
 }else{
-	
+
 	$sessionResponse = 'error';
 	$sessionResponseCode = $t->responseCode;
 }
-
 
 ?>
