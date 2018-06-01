d2b2aee4da8d155af70a817b63734c22a4e7c6b3
James <jamejo@gmail.com>
2018-06-01 12:49:21 -0400

    Update traccarTime.php

--- web-examples/traccarTime.php
+++ web-examples/traccarTime.php
@@ -1,32 +1,16 @@
 <?php
-/*
- * Copyright 2018 James Joseph (james@yodiyil.com)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 class traccarTime {
 
-    public static function toIso($time, $timeZone) {
-        $time = strtotime($time);
-        $dateInLocal = date("Y-m-d H:i:s", $time);
-        $dt = new DateTime($dateInLocal, new DateTimeZone($timeZone));
-        $dt->setTimezone(new DateTimeZone('UTC'));
-        $utcTime = $dt->format('Y-m-d H:i:s');
-        $returnObject = new DateTime($utcTime);
-        $returnIso = substr($returnObject->format(DateTime::ATOM),0,-6).'.000Z';
-        return $returnIso;
-    }
+	public static function toIso($time, $timeZone) {
+		$time = strtotime($time);
+		$dateInLocal = date("Y-m-d H:i:s", $time);
+		$dt = new DateTime($dateInLocal, new DateTimeZone($timeZone));
+		$dt->setTimezone(new DateTimeZone('UTC'));
+		$utcTime = $dt->format('Y-m-d H:i:s');
+		$returnObject = new DateTime($utcTime);
+		$returnIso = substr($returnObject->format(DateTime::ATOM), 0, -6) . '.000Z';
+		return $returnIso;
+	}
 
 }
 
