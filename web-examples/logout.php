36602344a607fc81d233c6874a093ee4e3164591
James <jamejo@gmail.com>
2018-06-05 12:26:08 -0400

    Update logout.php

--- web-examples/logout.php
+++ web-examples/logout.php
@@ -8,12 +8,10 @@ $rows = array();
 if ($login->responseCode == '204') {
 	$response = $login->response;
 	$userArray = json_decode($response, true);
-	$out['apiResponse'] = 'ok';
 	$out['apiResponseCode'] = $login->responseCode;
 	$out['response'] = $userArray;
 	$rows[] = $out;
 } else {
-	$out['apiResponse'] = 'error';
 	$out['apiresponseCode'] = $login->responseCode;
 	$out['response'] = $login->response;
 	$rows[] = $out;
