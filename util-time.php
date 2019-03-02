2115710bb9881375d8982f8f1a262e15157dfc28
James <jamejo@gmail.com>
2019-03-02 10:04:16 -0500

    Update util-time.php

--- util-time.php
+++ util-time.php
@@ -1,24 +1,20 @@
 <?php
-class traccarTime {
 
-	public static function toIso($time, $timeZone) {
-		$time = strtotime($time);
-		$dateInLocal = date("Y-m-d H:i:s", $time);
-		$dt = new DateTime($dateInLocal, new DateTimeZone($timeZone));
-		$dt->setTimezone(new DateTimeZone('UTC'));
-		$utcTime = $dt->format('Y-m-d H:i:s');
-		$returnObject = new DateTime($utcTime);
-		$returnIso = substr($returnObject->format(DateTime::ATOM), 0, -6) . '.000Z';
-		return $returnIso;
-	}
-
+function toUTC($dateTime, $timeZone){
+	$dateInLocal = date("Y-m-d H:i:s", $dateTime);
+	$dt = new DateTime($dateInLocal, new DateTimeZone($timeZone));
+	$dt->setTimezone(new DateTimeZone('UTC'));
+	$utcTime = $dt->format('Y-m-d H:i:s');
+	$returnObject = new DateTime($utcTime);
+	$returnIso = substr($returnObject->format(DateTime::ATOM), 0, -6) . '.000Z';
+	return $returnIso;
 }
 
 /*
  * ReadMe / Instructions & Notes
  *
- * Date time Format to submit time in toIso function :  2017-07-25 00:00:10
+ * $dateTime value = epoch time format
  * Format of time zone :  Asia/Kolkata
- * This function takes the user input time and timezone and converts it into UTC time and returns a response ISO Time format.
+ * This function takes the users input time and timezone and converts it into UTC time and returns a response ISO Time format as required by traccar.
 */
 ?>
